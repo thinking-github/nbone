63d98814e5e3504034732e0addda4ec450d932af
ChenYiCheng0 <chenyicheng00@gmail.com>
2016-04-01 17:52:25 +0800

    jackson

---
+++ nbone/src/main/java/org/nbone/util/json/jackson/CustomDateDeSerializer.java
@@ -0,0 +1,28 @@
+package org.nbone.util.json.jackson;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.DeserializationContext;
+import org.codehaus.jackson.map.deser.std.StdDeserializer;
+/**
+ * reference  org.codehaus.jackson <br>
+ * JSON String to JavaObject <br>
+ * @author thinking 2012-9-25
+ * @see StdDeserializer
+ *
+ */
+public  class CustomDateDeSerializer extends CustomAbstractDateDeSerializer<Date>{
+  
+  protected CustomDateDeSerializer(Class<Date> vc,String[] formats) {
+    super(vc, formats);
+  }
+
+  public Date deserialize(JsonParser parser, DeserializationContext arg1)throws IOException, JsonProcessingException
+ {
+    return super.deserialize(parser, arg1);
+  }
+  
+}
