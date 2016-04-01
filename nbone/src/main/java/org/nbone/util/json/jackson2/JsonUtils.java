15dd75a11198c39b96e996709fc628b7b8726035
ChenYiCheng0 <chenyicheng00@gmail.com>
2016-04-01 18:17:19 +0800

    add jackson2

---
+++ nbone/src/main/java/org/nbone/util/json/jackson2/JsonUtils.java
@@ -0,0 +1,16 @@
+package org.nbone.util.json.jackson2;
+
+import org.nbone.constant.DateConstant;
+
+/**
+ * 使用 jackson2
+ * base by com.fasterxml.jackson.databind.ObjectMapper
+ * @author thinking  2014-04-26
+ * @version 1.0
+ * @since 2014-04-26
+ */
+public class JsonUtils implements DateConstant{
+	
+	
+	
+}
